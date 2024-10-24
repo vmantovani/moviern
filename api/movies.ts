8c7cb7e6d3cba66a899b1f1e21d071408091f443
Vinicius Mantovani <vmantovani86@gmail.com>
2024-10-24 15:20:00 +0200

    adjust env variables for API

--- api/movies.ts
+++ api/movies.ts
@@ -1,11 +1,18 @@
 
 export const fetchTopRatedMovies = async () => {
+
+    const apiToken = process.env.EXPO_PUBLIC_MOVIE_API_TOKEN
+
+    if (!apiToken) {
+      throw new Error('API token is not defined');
+    }
+
     const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
     const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjc2YzkxMmJlMzI4OTAyZmY3ZDQ1NWMwN2Q5MDE0YiIsIm5iZiI6MTcyOTYxMDE3Ny43NjAzODcsInN1YiI6IjY3MTdiZTFmZGNiN2ZmODExZTQ3NzU4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9u9JtQCXuseqodJqvdVlo0Hms_ZP69FhD_fzXn9y3co'
+      Authorization: apiToken
   }
 };
 
